e6fb56e62c3fb1c226d0c8e7bc65ac77818bb27d
k1dave6412 <k1dave6412@gmail.com>
2017-06-19 12:21:30 +0800

    api

--- CountDebtv1/app/src/main/java/com/example/k1dave6412/countdebtv1/MainActivity.java
+++ CountDebtv1/app/src/main/java/com/example/k1dave6412/countdebtv1/MainActivity.java
@@ -3,45 +3,81 @@ package com.example.k1dave6412.countdebtv1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import com.shephertz.app42.gaming.multiplayer.client.WarpClient;
+import com.shephertz.app42.gaming.multiplayer.client.command.WarpResponseResultCode;
+import com.shephertz.app42.gaming.multiplayer.client.events.ConnectEvent;
+import com.shephertz.app42.gaming.multiplayer.client.listener.ConnectionRequestListener;
+
 
 public class MainActivity extends AppCompatActivity {
-
     Button start, rule;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WarpClient.initialize("028787cc650776dc57338c958c98e1db04e83f9f22747ae90f70f350e153bd8b", "2f4a6eef4702d8afb9e78892bc971be38f020b85e4009c5b61b96102da42bfd9");
 
-        start = (Button)findViewById(R.id.start);
-        rule = (Button)findViewById(R.id.rule);
+        start = (Button) findViewById(R.id.start);
+        rule = (Button) findViewById(R.id.rule);
 
-        start.setOnClickListener(new Button.OnClickListener(){
+        start.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,GameActivity.class);
+                Intent i = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(i);
                 MainActivity.this.finish();
             }
         });
 
-        rule.setOnClickListener(new Button.OnClickListener(){
+        rule.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,IntroActivity.class);
+                Intent i = new Intent(MainActivity.this, IntroActivity.class);
                 startActivity(i);
             }
         });
 
     }
+
+    /*
+    3Defining Listener Callbacks:It is recommended that you should implement request
+    listeners which will be called by WarpClient with the result of the requests. WarpClient
+     comprises different listeners for different type of requests. Here is a simple implementation
+     of connection request listener.
+
+    public abstract class MyConnectionListener implements ConnectionRequestListener {
+        @Override
+        public void onConnectDone(ConnectEvent event) {
+            if (event.getResult() == WarpResponseResultCode.SUCCESS) {
+                System.out.println("yipee I have connected");
+            }
+        }
+
+        @Override
+        public void onDisconnectDone(ConnectEvent event) {
+            System.out.println("On Disconnected invoked");
+        }
+    }
+    */
+
+    /*
+    4Adding Listeners.Once the request listeners are implemented,
+    they need to be added to WarpClient instance as follows:
+    WarpClient myGame = WarpClient.getInstance();
+    myGame.addConnectionRequestListener(new
+    MyConnectionListener());
+    */
+
+    /*5Connecting to the AppWarp Server:Once you have supplied all the listeners you just need
+    to call the connectWithUserName() method of WarpClient to connect to AppWarp server and
+    join the Zone by giving in the user name as the parameter with which the client wishes
+    to join the online application.
+
+    WarpClient.getInstance().connectWithUserName("Jonathan");    
+     */
 
 }
---
+++ android-and-java-sdk/README.md
@@ -0,0 +1,26 @@
+AppWarp_JAVA_SDK
+==============
+
+
+1. [Register] (https://apphq.shephertz.com/register) with AppWarp platform.
+2. Create an app once you are on Quick start page after registration.
+3. If you are already registered, login to [AppHQ] (http://apphq.shephertz.com/) console and create an app from App Manager Tab.
+
+__Download and Set up SDK :-__
+
+1). [Download] (http://appwarp.shephertz.com/wp-content/themes/twentytwelve/download/android-and-java-sdk.zip) Java SDK
+
+2). Unzip downloaded Zip file.
+
+3). Folder will contain App42MultiPlayerGamingSDK-x.x.x.jar,other required jar file in lib folder and docs.
+
+4). If you are making new project then add App42MultiPlayerGamingSDK-x.x.x.jar and other required jar file in lib folder.
+
+ or 
+ 
+4). If your using your existing project then add App42MultiPlayerGamingSDK-x.x.x.jar and lib jars in your project lib folder.
+ 
+5).Put these jar files in class path of your Java project.
+
+6). API USAGE GUIDE (http://appwarp.shephertz.com/game-development-center/java-game-developers-home/java-usage-guide/)
+
